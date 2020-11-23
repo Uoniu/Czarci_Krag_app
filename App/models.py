37fd0826c5e1dcd83cb18c82aec340c58e611bfb
Mateusz Łoniewski <mateusz.loniewski@student.wat.edu.pl>
2020-11-23 13:26:40 +0100

    adding data models

--- App/models.py
+++ App/models.py
@@ -8,3 +8,50 @@ class Test(models.Model):
 
     def __str__(self):
         return self.col
+
+
+class Aktualnosci(models.Model):
+    Naglowek = models.CharField(max_length=50)
+    Tresc = models.CharField(max_length=100)
+    Istotnosc = models.IntegerField()
+
+
+class FAQ (models.Model):
+    Tytul = models.CharField(max_length=50)
+    Tresc = models.CharField(max_length=200)
+    Odpowiedz = models.CharField(max_length=200)
+
+
+class Buty (models.Model):
+    Rozmiar = models.IntegerField()
+    Id_Rezerwacji = models.ForeignKey("Rezerwacja",on_delete=models.SET_NULL,null=True)
+
+
+class Rezerwacja (models.Model):
+    DataRozpoczecia = models.DateTimeField()
+    DataZakonczenia = models.DateTimeField()
+    IdUzytkownika = models.ForeignKey("Uzytkownik",on_delete=models.SET_NULL,null=True)
+    Uwagi = models.CharField(max_length=50)
+    IdUslugi = models.ForeignKey("Uslugi",on_delete=models.SET_NULL,null=True)
+
+
+class Uslugi(models.Model):
+    NazwaUslugi = models.CharField(max_length=30,null=False)
+    TypUslugi = models.CharField(max_length=20)
+
+class Uzytkownik(models.Model):
+    TypUzytkownika = models.CharField(max_length=20)
+    Imie = models.CharField(max_length=50)
+    Nazwisko = models.CharField(max_length=80)
+    Haslo = models.CharField(max_length=300)
+    email = models.CharField(max_length=100)
+    NrTelefonu = models.CharField(max_length=15)
+
+class Galeria (models.Model):
+    Tytul = models.CharField(max_length=30)
+    Zdjecie = models.FileField()
+
+class ProgramLojalnosciowy (models.Model):
+    Nagroda = models.CharField(max_length=30)
+    Punkty = models.IntegerField()
+    IdUzytkownika = models.ForeignKey("Uzytkownik",on_delete=models.SET_NULL,null=True)
